8823b3d9edeadf8dfa6b1022bc2abdd7c2031890
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-03-11 17:07:20 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader@13793 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- browser/src/jd/http/HTTPConnectionFactory.java
+++ browser/src/jd/http/HTTPConnectionFactory.java
@@ -6,6 +6,7 @@ public class HTTPConnectionFactory {
 
     public static URLConnectionAdapter createHTTPConnection(final URL url, final HTTPProxy proxy) {
         if (proxy == null) { return new HTTPConnection(url); }
+        if (proxy.getType().equals(HTTPProxy.TYPE.NONE)) { return new HTTPConnection(url); }
         if (proxy.getType().equals(HTTPProxy.TYPE.DIRECT)) { return new HTTPConnection(url, proxy); }
         if (proxy.getType().equals(HTTPProxy.TYPE.SOCKS5)) { return new Socks5HTTPConnection(url, proxy); }
         if (proxy.getType().equals(HTTPProxy.TYPE.HTTP)) { return new HTTPProxyHTTPConnection(url, proxy); }
